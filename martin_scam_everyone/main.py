78c935f621b25153a7172fc84eedf6d530dc0c7d
smncu-pace <smncu.zc@gmail.com>
2025-10-23 11:52:46 +0800

    ovotim

--- martin_scam_everyone/main.py
+++ martin_scam_everyone/main.py
@@ -1,7 +1,7 @@
 import webbrowser
 from .utils import welcome
 def main():
-    webbrowser.open("https://www.bilibili.com/video/BV1UT42167xb/?spm_id_from=333.337.search-card.all.click")
+    webbrowser.open("https://www.bilibili.com/video/BV1Q9gAzuEpi/?spm_id_from=333.337.search-card.all.click")
     welcome()
 if __name__ == "__main__":
     main()
